046f34e3e635fee2020406269d70eea1c9aeea0e
ZhangZiHao_Home <nbhaohao@163.com>
2018-09-09 16:57:29 +0800

    创建 ButtonGroup 组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './buttonGroup'
 
 Vue.component('p-icon', Icon)
 Vue.component('p-button', Button)
+Vue.component('p-button-group', ButtonGroup)
 
 new Vue({
     el: '#app',
