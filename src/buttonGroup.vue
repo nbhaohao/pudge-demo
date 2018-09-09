a897add60c31266e02f53501f29f909fb3b3572d
ZhangZiHao_Home <nbhaohao@163.com>
2018-09-09 18:57:03 +0800

    修改 ButtonGroup 中会将第一个元素向右偏移的问题

--- src/buttonGroup.vue
+++ src/buttonGroup.vue
@@ -29,7 +29,9 @@
         vertical-align: middle;
         > .p-button {
             border-radius: 0;
-            margin-left: -1px;
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
             &:first-child {
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
